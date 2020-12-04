ec06c83b196d01d18fd10090e85b31455e5ce96c
Roger Howell <howelrtc@aston.ac.uk>
2020-12-04 17:20:27 +0000

    fixed gradleModuleRoot to dynamically determine the number of parent directories to traverse (variations caused by running the jar versus running the sources via your IDE)

--- src/main/java/com/yourorganization/maven_sample/Util.java
+++ src/main/java/com/yourorganization/maven_sample/Util.java
@@ -11,16 +11,33 @@ public class Util {
     /**
      * Temporary addition until JavaParser itself contains a similar method.
      * This is almost identical to the maven equivalent at CodeGenerationUtils#mavenModuleRoot()
-     *
+     * <p>
      * Useful for locating source code within your Gradle project. Finds the classpath for the given class,
-     * then backs up out of multiple directories, until reaching the directory containing build.gradle
-     * "build/(test-)classes/java/main"
+     * then backs up out of multiple directories, until reaching the directory containing build.gradle -
+     * typically this is "build/(test-)classes/java/main" (four traversals)
+     * or "build/libs/javaparser-gradle-sample-shadow-1.0-SNAPSHOT" (three)
      */
     public static Path gradleModuleRoot(Class<?> c) {
-        return CodeGenerationUtils
+        String buildFileName = "build.gradle";
+
+
+        Path other = Paths.get(".");
+        Path normalize = CodeGenerationUtils
                 .classLoaderRoot(c)
-                .resolve(Paths.get("..", "..", "..", ".."))
+                .resolve(other)
                 .normalize();
+
+        // If it's not a directory, or if it is a directory and the build file isn't present, go up a level.
+        while (!normalize.toFile().isDirectory() || !normalize.resolve(buildFileName).toFile().exists()) {
+            System.out.println("source root not found at + " + normalize + ", trying next directory up");
+            other = other.resolve("..");
+            normalize = CodeGenerationUtils
+                    .classLoaderRoot(c)
+                    .resolve(other)
+                    .normalize();
+        }
+
+        return normalize;
     }
 
 }
